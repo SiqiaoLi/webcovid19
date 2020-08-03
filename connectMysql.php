cf9e18e3f03ee873b6e00fd9c94bef0fd3a245c4
SiqiaoLi <48227782+SiqiaoLi@users.noreply.github.com>
2020-08-03 20:32:17 +1000

    Delete connectMysql.php

--- connectMysql.php
+++
@@ -1,4 +0,0 @@
-<?php 
-//$con = new mysqli(null, "ccmysql", "ccmysql", "user", null, "/cloudsql/s3774940-cc2018:australia-southeast1:ccmysql");
-$con = new mysqli(null, "ccmysql", "ccmysql", "user", null, "/cloudsql/task2-272810:australia-southeast1:ccassignment-mysql");
-?>
